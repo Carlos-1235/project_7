1aeb04a8b067303c70c01b260410689307689c06
Carlos Ayala <you@example.com>
2025-10-24 18:06:22 -0600

    Add project files and EDA notebook

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project_7
 Tripleten project #7
+La app consta de 2 botones donde el primero es para crear un histograma del conjunto de datos de anuncios de venta de coches y el segundo para crear un grafico de dispercion del mismo conjunto de datos
---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv')
+st.header('Construir histograma')
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
